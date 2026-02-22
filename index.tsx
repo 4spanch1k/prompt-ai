d6295ffbd639a8e2c5ae87dbe89c0355d7f0c35f
4spanch1k <aspanfx@gmail.com>
2026-02-22 22:42:31 +0500

    upd

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import './src/styles/variables.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
